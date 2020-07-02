1db677b418602fb66d79dea25ebbaddf625b5ce2
annesoflouret <annesophieflouret@gmail.com>
2020-07-02 15:52:15 +0200

    add function

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,5 +1,12 @@
-const confirmationId = localStorage.getItem("orderConfirmation");
-const confirmation = document.getElementById("camera");
-const messageConfirmation = document.createElement("p");
-messageConfirmation.innerHTML = "Nous vous remercions pour votre commande n° "+ localStorage.orderConfirmation;
-confirmation.appendChild(messageConfirmation);
+/////////////////////////////////////// Confirmation de la commade /////////////////////////////////
+function addConfirmationText(){
+    const confirmationId = localStorage.getItem("orderConfirmation");
+    const confirmation = document.getElementById("camera");
+    const messageConfirmation = document.createElement("p");
+    messageConfirmation.innerHTML = "Nous vous remercions pour votre commande n° "+ localStorage.orderConfirmation;
+    confirmation.appendChild(messageConfirmation);
+}
+
+addConfirmationText();
+
+
