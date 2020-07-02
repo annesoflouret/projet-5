e35748483d416c28c3f210dd65bb0ed3f3674f05
annesoflouret <annesophieflouret@gmail.com>
2020-07-02 15:53:21 +0200

    change == to === and add function

--- js/orinoco.js
+++ js/orinoco.js
@@ -1,28 +1,24 @@
-get("http://localhost:3000/api/cameras").then( function(response){
-    const section = document.getElementsByClassName("row");
-    
-    //création des cardes de présentation des appareils photos
-    for (let i = 0; i < response.length; i = i + 1)
-    {   
-        const div = document.createElement("div");
-        div.innerHTML = response[i].name;
+//////////////////////////////////// Ajoute les informations d'un produit dans le code HTML ////////////////////////
+function addProduct(responseProduct, section){
+    const div = document.createElement("div");
+        div.innerHTML = responseProduct.name;
         div.setAttribute("class", "col-md-5 product-border mt-5 mb-4 col-sm-6 mr-4 ml-4");
 
         const img = document.createElement("img");
-        img.setAttribute("src", response[i].imageUrl);
+        img.setAttribute("src", responseProduct.imageUrl);
         img.setAttribute("width", "100%");
 
         const legend = document.createElement("div");
-        legend.innerHTML = response[i].description;
+        legend.innerHTML = responseProduct.description;
 
         const lenses = document.createElement("p");
-        lenses.innerHTML = "Choix des optiques: "+ response[i].lenses;
+        lenses.innerHTML = "Choix des optiques: "+ responseProduct.lenses;
         
         const price = document.createElement("p");
-        price.innerHTML = response[i].price + "€";
+        price.innerHTML = responseProduct.price + "€";
 
         const link = document.createElement("a");
-        link.setAttribute("href", "produit.html?id=" + response[i]._id);
+        link.setAttribute("href", "produit.html?id=" + responseProduct._id);
 
         section[1].appendChild(div);
         div.appendChild(link);
@@ -30,14 +26,28 @@ get("http://localhost:3000/api/cameras").then( function(response){
         div.appendChild(legend);
         div.appendChild(lenses);
         div.appendChild(price);
-    }
+}
+
+//////////////////////////////////////// Ajoute une div //////////////////////////////////////////////////////////
+function addDivToFixDisplay(section){
+    const div = document.createElement("div");
+    div.setAttribute("class", "col-md-5 mt-5 mb-4");
+    section[1].appendChild(div);
+}
+
+
+get("http://localhost:3000/api/cameras").then( function(response){
+    const section = document.getElementsByClassName("row");
     
-    //ajoute une div quand le nombre d'élément est impair
-    //modulo 2 = le reste de la division par 2, exemple 3/2 = 1, reste 1
-    if (response.length % 2 == 1) {
-        const div = document.createElement("div");
-        div.setAttribute("class", "col-md-5 mt-5 mb-4");
-        section[1].appendChild(div);
+    ///////////////////////////// Création des cardes de présentation des appareils photos ////////////////////////
+    for (let i = 0; i < response.length; i = i + 1)
+    {   
+        addProduct(response[i], section);
+    }
+    ////////////////////////////////////////Ajoute une div quand le nombre d'élément est impair/////////////////
+    ////////////////////modulo 2 = le reste de la division par 2, exemple 3/2 = 1, reste 1
+    if (response.length % 2 === 1) {
+        addDivToFixDisplay(section);
     }
     
 }).catch(function(err){
