ffafd91f1b1aaa173d7799b246515cf9738325a3
annesoflouret <annesophieflouret@gmail.com>
2020-07-02 14:51:28 +0200

    routine

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,6 @@
 function get(url){
     const promise = new Promise(function(resolve, reject){
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
         request.open("GET", url);
         request.onreadystatechange = function(){
             if (this.readyState === XMLHttpRequest.DONE){
@@ -18,7 +18,7 @@ function get(url){
 
 function post(url, jsonBody){
     const promise = new Promise(function(resolve, reject){
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
         request.open("POST", url);
         request.setRequestHeader("Content-Type", "application/json");
         request.onreadystatechange = function () {
