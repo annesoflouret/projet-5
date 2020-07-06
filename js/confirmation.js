62a6cf550f00f70b2f42cc4b94332bdb410cf899
annesoflouret <annesophieflouret@gmail.com>
2020-07-06 14:27:58 +0200

    add total price on confirmation page

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,11 +1,15 @@
 /////////////////////////////////////// Confirmation de la commade /////////////////////////////////
 function addConfirmationText(){
-    const confirmationId = localStorage.getItem("orderConfirmation");
+    const confirmationId = localStorage.getItem("orderConfirmation"); // récuperation de l'ID dans le local storage
+    const totalPrice = localStorage.getItem("totalPriceConfirmationPage"); //récuperation du prix dans le local storage
     const confirmation = document.getElementById("confirmation");
     const messageConfirmation = document.createElement("p");
-    messageConfirmation.innerHTML = "Nous vous remercions pour votre commande n° "+ localStorage.orderConfirmation;
+    const confirmationPrice = document.createElement("p");
+    messageConfirmation.innerHTML = "Nous vous remercions pour votre commande n° "+ confirmationId;
+    confirmationPrice.innerHTML = "Prix total de votre commande: "+ totalPrice + "€";
     messageConfirmation.setAttribute("class", "confirmation-title pt-5")
     confirmation.appendChild(messageConfirmation);
+    confirmation.appendChild(confirmationPrice);
 }
 
 addConfirmationText();
